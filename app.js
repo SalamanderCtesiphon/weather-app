19b131c6e8c9b2fd36e455567e674b21420e145c
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-27 14:54:39 -0500

    learinng about asynchronous code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-console.log('hi');
 
-const img = document.querySelector('img');
+
     
 async function getWeather() {
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b37330c70fce4bd19ce153856232703&q=79707&aqi=no`, {mode: 'cors'});
-  const weatherData = await response.json();
-  console.log(weatherData);     
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b37330c70fce4bd19ce153856232703&q=midland,texas&aqi=no`, {mode: 'cors'});
+  const weatherData = await response.json(); 
+  console.log(weatherData);  
 }
 getWeather();  
+
