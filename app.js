16fad16c52a3029d2451afd9dd89ca9ffa7d557b
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-28 11:20:18 -0500

    added a funciton to clear the page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function onSubmit(e) {
   e.preventDefault();
   const searchItem = document.getElementById('search').value;
   userLocation = searchItem;
+  clearPage();
   getWeather();
 }
  
@@ -16,13 +17,19 @@ async function getWeather() {
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b37330c70fce4bd19ce153856232703&q=${userLocation}&aqi=no`, {mode: 'cors'});
   const weatherData = await response.text(); 
   const weatherObj = JSON.parse(weatherData);
-  console.log(weatherObj.current.condition.icon); 
+  console.log(weatherObj); 
   displayIcon(weatherObj); 
 } 
 
 function displayIcon(weatherObj) {
   const pageView = document.querySelector('.pageView');
   const weatherIcon = document.createElement('img');
+  weatherIcon.innerHTML = '';
   weatherIcon.src = weatherObj.current.condition.icon;
   pageView.appendChild(weatherIcon);
+}
+
+function clearPage() {
+  const pageView = document.querySelector('.pageView');
+  pageView.innerHTML = '';
 }
