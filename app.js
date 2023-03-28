c1086a86bc88b16feab565dea460a7b30994ac8c
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-28 10:15:02 -0500

    found a way to isolate and use the returned JSON datas

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
     
 async function getWeather() {
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b37330c70fce4bd19ce153856232703&q=midland,texas&aqi=no`, {mode: 'cors'});
-  const weatherData = await response.json(); 
-  console.log(weatherData);  
+  const weatherData = await response.text(); 
+  const weatherObj = JSON.parse(weatherData);
+  console.log(weatherObj.current.temp_f);  
 }
 getWeather();  
 
