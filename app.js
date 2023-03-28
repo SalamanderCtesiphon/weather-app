2f44fee475b97b66324ff1c35e7996b0e3689e1b
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-28 10:33:10 -0500

    added a function that allows the user to submit their location to the weather api

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 
 
 let userLocation = "79701";
+
+const submitBtn = document.getElementById('submitButton');
+submitBtn.addEventListener('click', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const searchItem = document.getElementById('search').value;
+  userLocation = searchItem;
+  getWeather();
+}
  
 async function getWeather() {
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=b37330c70fce4bd19ce153856232703&q=${userLocation}&aqi=no`, {mode: 'cors'});
   const weatherData = await response.text(); 
   const weatherObj = JSON.parse(weatherData);
   console.log(weatherObj.current.temp_f);  
-}
-getWeather();  
+} 
 
