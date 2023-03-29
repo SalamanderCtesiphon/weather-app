7b6909aea16c7474f71b43de4ca32eab319d61eb
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-29 09:58:07 -0500

    added a condole log to the main function so that I can explain how to deconstruct the object to other people

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ async function getWeather() {
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b37330c70fce4bd19ce153856232703&q=${userLocation}&aqi=no`, {mode: 'cors'});
   const weatherData = await response.text(); 
   const weatherObj = JSON.parse(weatherData); 
+  console.log(weatherObj);
   displayIcon(weatherObj); 
 } 
 
